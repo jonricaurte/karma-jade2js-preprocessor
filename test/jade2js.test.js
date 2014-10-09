b01f71505a3a72114a4804287a77e03dde16edcf
Tom Buchok <tom@buchok.com>
2014-10-09 09:28:55 -0400

    adds mocha and expect.js matchers

--- test/jade2js.test.js
+++ test/jade2js.test.js
@@ -1,29 +1,38 @@
-var assert = require('assert');
-var util = require('util');
+var expect = require('expect.js');
 var jade2js = require('../lib/jade2js');
-var loggerStub = { create: function(){ return { debug: function() {} } } };
-var basePathStub = 'foo';
-var fileStub = { originalPath: '/tmp/original/path', path: '/tmp/new/path' }
-var testConfig = { locals: { message: 'Hello world' } };
-var compileFn = jade2js(loggerStub, basePathStub, testConfig);
-var successes = 0;
-var expected = 2;
+var LOGGER_STUB = { create: function(){ return { debug: function() {} } } };
+var BAST_PATH_STUB = 'foo';
+var FILE_STUB = { originalPath: '/tmp/original/path', path: '/tmp/new/path' }
+var TEST_CONFIG = { locals: { message: 'Hello world' } };
 
-compileFn('h1 This is a bland message', fileStub, function(result) {
-  assert.ok(result, 'it returns a proper message');
-  successes += 1;
+describe('jade2js', function() {
+  describe('default behavior [no config]', function() {
+    var compileFn = jade2js(LOGGER_STUB, BAST_PATH_STUB);
+    var html;
+    before(function(done) {
+      var JADE_SNIPPET = 'h1 This is a bland message'
+      compileFn(JADE_SNIPPET, FILE_STUB, function(result) {
+        html = result;
+        done();
+      });
+    });
+    it('should succeed', function() {
+      expect(html).to.be.ok();
+    });
+  });
+  describe('config object', function() {
+    var compileFn = jade2js(LOGGER_STUB, BAST_PATH_STUB, TEST_CONFIG);
+    var html;
+    before(function(done) {
+      var JADE_SNIPPET = 'h1= message';
+      compileFn(JADE_SNIPPET, FILE_STUB, function(result) {
+        html = result;
+        done();
+      });
+    });
+    it('should contain the `locals` data', function() {
+      expect(html).to.contain('Hello world');
+    });
+  });
 });
-compileFn('h1= message', fileStub, function(result) {
-  assert.ok(/hello world/i.test(result), 'it contains the locals');
-  successes += 1;
-});
-
-
-var checkComplete = function() {
-  if (successes == expected)
-    console.log('all tests pass!');
-  else
-    process.nextTick(checkComplete);
-}
-process.nextTick(checkComplete);
 
